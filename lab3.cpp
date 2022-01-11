f62a489f6e5409922e599643738dec7204a3e927
Geronimo310 <94980729+Geronimo310@users.noreply.github.com>
2022-01-11 09:58:29 +0300

    Update lab3.cpp

--- lab3.cpp
+++ lab3.cpp
@@ -3,79 +3,88 @@
 
 class Animal
 {
-protected:
-    std::string sound;
 public:
+    std::string sound, animal;
     Animal()
-    {   
-        std::cout << "Constructor called for class 'Animal'." << std::endl;
+    {
+        std::cout << "Constructor called." << std::endl;
+    }
+    void virtual Print()
+    {
+        std::cout << animal << " says " << sound << "." << std::endl;
     }
 };
 
-class Cat: protected Animal
+class Cat : public Animal
 {
 public:
     Cat()
     {
+        animal = "Cat";
         sound = "meow";
-        this -> voice();
     }
-    void voice()
+    void Print()
     {
-        std::cout << "Cat says "  << sound << "." << std::endl;
+        std::cout << animal << " says " << sound << "." << std::endl;
     }
 };
 
-class Dog: protected Animal
+class Dog : public Animal
 {
 public:
     Dog()
-        {
-            sound = "woof";
-            this -> voice();
-        }
-        void voice()
-        {
-            std::cout << "Dog says "  << sound << "." << std::endl;
-        }
+    {
+        animal = "Dog";
+        sound = "woof";
+    }
+    void Print()
+    {
+        std::cout << animal << " says " << sound << "." << std::endl;
+    }
 };
 
 
-class Cow: protected Animal
+class Cow : public Animal
 {
 public:
     Cow()
-        {
-            sound = "moo";
-            this -> voice();
-        }
-        void voice()
-        {
-            std::cout << "Cow says "  << sound << "." << std::endl;
-        }       
+    {
+        animal = "Cow";
+        sound = "moo";
+    }
+
+    void Print()
+    {
+        std::cout << animal << " says " << sound << "." << std::endl;
+    }
 };
 
 
 int main()
 {
+    Animal animal1;
+
     int a;
-    std::cout <<"Choose your animal: 1 - cat; 2 - dog; 3 - cow." << std::endl;
+    std::cout << "Choose your animal: 1 - Cat; 2 - Dog; 3 - Cow." << std::endl;
     std::cin >> a;
-        if (a == 1) 
-        {
-            Cat Cat1;
-        }
-        else if (a == 2)
-        {
-            Dog Dog1;
-        }
-        else if (a == 3)
-        {
-            Cow Cow1;
-        }
-        else 
-        {
-            std::cout <<"Error! Wrong number.";
-        }
+    if (a == 1)
+    {
+        Animal* abstractAnimal = new Cat();
+        abstractAnimal->Print();
+    }
+    else if (a == 2)
+    {
+        Animal* abstractAnimal = new Dog();
+        abstractAnimal->Print();
+    }
+    else if (a == 3)
+    {
+        Animal* abstractAnimal = new Cow();
+        abstractAnimal->Print();
+    }
+    else
+    {
+        std::cout << "Error! Wrong number.";
+    }
     return 0;
 }
